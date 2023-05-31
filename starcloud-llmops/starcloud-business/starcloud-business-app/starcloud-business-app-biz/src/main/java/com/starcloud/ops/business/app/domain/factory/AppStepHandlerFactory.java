a1d790d39e607bdd3dcbbe22626a54290d652119
nacoyer <nacoyer@qq.com>
2023-05-31 17:04:39 +0800

    template

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/domain/factory/AppStepHandlerFactory.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/domain/factory/AppStepHandlerFactory.java
@@ -1,11 +1,13 @@
 package com.starcloud.ops.business.app.domain.factory;
 
 /**
+ * 获取步骤处理器工厂类
+ *
  * @author nacoyer
  * @version 1.0.0
  * @since 2023-05-31
  */
-public class AppStepFactory {
+public class AppStepHandlerFactory {
 
 
 }
