c555ab592a1ab6b17ff6af59a95a39c647eff416
df007df <duanfeisj@gmail.com>
2023-07-18 11:03:48 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/domain/entity/params/JsonParamsEntity.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/domain/entity/params/JsonParamsEntity.java
@@ -3,6 +3,9 @@ package com.starcloud.ops.business.app.domain.entity.params;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * jsonSchemas 结构的参数，后面做自动解析逻辑处理
+ */
 @Slf4j
 @Data
 public class JsonParamsEntity extends BaseParamsEntity {
