08cb4f310795121732282162cde44365f15a8381
fanmiao <aiden_fzq@163.com>
2023-08-18 10:33:59 +0800

    fix:matchQuery

--- starcloud-llmops/starcloud-business/starcloud-business-dataset/starcloud-business-dataset-api/src/main/java/com/starcloud/ops/business/dataset/pojo/request/MatchQueryRequest.java
+++ starcloud-llmops/starcloud-business/starcloud-business-dataset/starcloud-business-dataset-api/src/main/java/com/starcloud/ops/business/dataset/pojo/request/MatchQueryRequest.java
@@ -5,14 +5,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Data
 @Schema(description = "文件分段命中测试")
 public class MatchQueryRequest {
 
-    @Schema(description = "文本Id")
-    @NotBlank(message = "documentId 不能为空")
-    private String documentId;
+    @Schema(description = "数据集uid")
+    @NotBlank(message = "数据集uid 不能为空")
+    private List<String> datasetUid;
 
     @Schema(description = "匹配文本")
     @NotBlank(message = "文本内容不能为空")
