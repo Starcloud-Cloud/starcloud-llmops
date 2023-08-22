424876d0669b5b18cf707cf0b47a33424c36cfbe
17835411844@163.com <17835411844@163.com>
2023-08-22 16:33:37 +0800

    fix 数据集索引开启

--- starcloud-llmops/starcloud-business/starcloud-business-dataset/starcloud-business-dataset-biz/src/main/java/com/starcloud/ops/business/dataset/mq/consumer/DataSetSourceDataIndexSendConsumer.java
+++ starcloud-llmops/starcloud-business/starcloud-business-dataset/starcloud-business-dataset-biz/src/main/java/com/starcloud/ops/business/dataset/mq/consumer/DataSetSourceDataIndexSendConsumer.java
@@ -46,8 +46,6 @@ public class DataSetSourceDataIndexSendConsumer extends AbstractDataProcessor<Da
             // 创建索引
              documentSegmentsService.indexDoc(String.valueOf(message.getDatasetId()), String.valueOf(message.getDataSourceId()));
 
-            // Fixme 创建总结  总结如果创建失败 状态为 SUMMERY_COMPLETED code 98
-
             // 设置数据状态
             message.setStatus(DataSetSourceDataStatusEnum.COMPLETED.getStatus());
             message.setErrMsg(DataSetSourceDataStatusEnum.COMPLETED.getName());
