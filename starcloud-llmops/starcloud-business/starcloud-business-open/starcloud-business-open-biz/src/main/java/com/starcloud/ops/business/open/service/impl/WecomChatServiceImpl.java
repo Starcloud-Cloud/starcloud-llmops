f49f13525818464b204e745f4c4a7ec284d8ce8c
fanmiao <aiden_fzq@163.com>
2023-08-22 16:05:59 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-open/starcloud-business-open-biz/src/main/java/com/starcloud/ops/business/open/service/impl/WecomChatServiceImpl.java
+++ starcloud-llmops/starcloud-business/starcloud-business-open/starcloud-business-open-biz/src/main/java/com/starcloud/ops/business/open/service/impl/WecomChatServiceImpl.java
@@ -55,13 +55,15 @@ public class WecomChatServiceImpl implements WecomChatService {
         AppPublishChannelRespVO channelRespVO = appPublishChannelService.getAllByMediumUid(reqVO.getGroupRemark());
         if (channelRespVO == null) {
             sendMsg(reqVO.getGroupRemark(), "渠道绑定异常，请联系管理员", reqVO.getReceivedName());
+            return;
         }
 
         if (channelRespVO.getStatus() == null || channelRespVO.getStatus() != 0) {
             sendMsg(reqVO.getGroupRemark(), "此渠道已禁用", reqVO.getReceivedName());
+            return;
         }
 
-        String userNameMd5 = userNameMd5(reqVO.getReceivedName());
+        String userNameMd5 = userNameMd5(reqVO.getGroupRemark() + reqVO.getReceivedName());
         ChatRequestVO chatRequestVO = new ChatRequestVO();
         chatRequestVO.setAppUid(channelRespVO.getAppUid());
         chatRequestVO.setQuery(reqVO.getSpoken());
