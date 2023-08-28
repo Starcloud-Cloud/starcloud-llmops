a4003993c26d45c29c8df62be50d2b3703f12672
nacoyer <nacoyer@qq.com>
2023-08-28 16:18:30 +0800

    App Publish Limit

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/api/limit/vo/request/AppPublishLimitOperateReqVO.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/api/limit/vo/request/AppPublishLimitOperateReqVO.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
