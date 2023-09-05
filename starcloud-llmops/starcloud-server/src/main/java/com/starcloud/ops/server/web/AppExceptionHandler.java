7c6f293f3f372022b2316ebbfbccf0dbac73e395
nacoyer <nacoyer@qq.com>
2023-09-05 14:00:37 +0800

    App Publish Limit

--- starcloud-llmops/starcloud-server/src/main/java/com/starcloud/ops/server/web/AppExceptionHandler.java
+++ starcloud-llmops/starcloud-server/src/main/java/com/starcloud/ops/server/web/AppExceptionHandler.java
@@ -7,8 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * 全局异常处理器，将 Exception 翻译成 CommonResult + 对应的异常编号
  *
