7e117e1d2843f05a79befd0533b19e98fa50afc5
fanmiao <aiden_fzq@163.com>
2023-09-06 13:45:18 +0800

    fix: 聊天分享

---
+++ starcloud-llmops/starcloud-business/starcloud-business-share/src/main/java/com/starcloud/ops/business/share/dal/dataobject/ShareConversationDO.java
@@ -0,0 +1,56 @@
+package com.starcloud.ops.business.share.dal.dataobject;
+
+
+import cn.iocoder.yudao.framework.tenant.core.db.TenantBaseDO;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@TableName("llm_share_conversation")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class ShareConversationDO extends TenantBaseDO {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    /**
+     * uid
+     */
+    private String uid;
+
+    /**
+     * key
+     */
+    private String shareKey;
+
+    /**
+     * app uid
+     */
+    private String appUid;
+
+    /**
+     * 会话uid
+     */
+    private String conversationUid;
+
+    /**
+     * 过期时间
+     */
+    private LocalDateTime expiresTime;
+
+    /**
+     * 启用/禁用
+     */
+    private Boolean disabled;
+
+    /**
+     * 游客
+     */
+    private Boolean endUser;
+
+
+}
