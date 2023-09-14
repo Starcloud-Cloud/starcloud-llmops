996f32bc25f700a7e5e0776b6d91d395a211b980
fanmiao <aiden_fzq@163.com>
2023-09-14 15:37:23 +0800

    es 参数校验

--- starcloud-llmops/starcloud-business/starcloud-business-dataset/starcloud-business-dataset-api/src/main/java/com/starcloud/ops/business/dataset/pojo/request/BaseQueryRequest.java
+++ starcloud-llmops/starcloud-business/starcloud-business-dataset/starcloud-business-dataset-api/src/main/java/com/starcloud/ops/business/dataset/pojo/request/BaseQueryRequest.java
@@ -3,6 +3,8 @@ package com.starcloud.ops.business.dataset.pojo.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 
@@ -15,8 +17,11 @@ public abstract class BaseQueryRequest {
     private String text;
 
     @Schema(description = "搜索文档数",defaultValue = "2")
-    private Long k ;
+    @Max(value = 5, message = "k 最大值为5")
+    private Long k;
 
     @Schema(description = "最低打分")
+    @Max(value = 1, message = "minScore 最大值为1")
+    @Min(value = 0, message = "minScore 最小值为0")
     private Double minScore;
 }
