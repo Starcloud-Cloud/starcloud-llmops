5047845bc146cf3eac1856f659f3ebc89eeefcab
fanmiao <aiden_fzq@163.com>
2023-09-14 13:58:21 +0800

    代理

--- starcloud-llmops/starcloud-business/starcloud-business-dataset/starcloud-business-dataset-biz/src/main/java/com/starcloud/ops/business/dataset/controller/admin/segment/SegmentController.java
+++ starcloud-llmops/starcloud-business/starcloud-business-dataset/starcloud-business-dataset-biz/src/main/java/com/starcloud/ops/business/dataset/controller/admin/segment/SegmentController.java
@@ -75,18 +75,4 @@ public class SegmentController {
         return CommonResult.success(true);
     }
 
-
-    @GetMapping("/split/enable/{datasetId}/{documentId}")
-    @Operation(summary = "文档分段禁用/启用", description = "文档分段禁用/启用")
-    public CommonResult<Boolean> test(
-            @PathVariable("datasetId") String datasetId,
-            @PathVariable("documentId") String documentId,
-            @RequestParam(value = "disable") boolean enable
-
-    ) {
-        documentSegmentsService.indexDoc(datasetId, documentId);
-        return null;
-    }
-
-
 }
