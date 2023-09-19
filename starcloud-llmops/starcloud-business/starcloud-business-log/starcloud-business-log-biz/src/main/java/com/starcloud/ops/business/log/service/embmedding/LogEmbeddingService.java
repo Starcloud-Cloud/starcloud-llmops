7962b138485f40bf264e9ff2d886651ca319cc72
fanmiao <aiden_fzq@163.com>
2023-09-19 09:36:20 +0800

    embedding日志

---
+++ starcloud-llmops/starcloud-business/starcloud-business-log/starcloud-business-log-biz/src/main/java/com/starcloud/ops/business/log/service/embmedding/LogEmbeddingService.java
@@ -0,0 +1,8 @@
+package com.starcloud.ops.business.log.service.embmedding;
+
+import com.starcloud.ops.business.log.dal.dataobject.LogEmbeddingDO;
+
+public interface LogEmbeddingService {
+
+    void createLog(LogEmbeddingDO logEmbeddingDO);
+}
