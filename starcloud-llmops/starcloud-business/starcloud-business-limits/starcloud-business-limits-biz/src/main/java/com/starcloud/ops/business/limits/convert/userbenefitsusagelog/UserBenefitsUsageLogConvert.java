981fa156d98da932f6dba8a221b0e8abf33c7474
17835411844@163.com <17835411844@163.com>
2023-09-21 15:27:00 +0800

    扣除权益下的用户修改

--- starcloud-llmops/starcloud-business/starcloud-business-limits/starcloud-business-limits-biz/src/main/java/com/starcloud/ops/business/limits/convert/userbenefitsusagelog/UserBenefitsUsageLogConvert.java
+++ starcloud-llmops/starcloud-business/starcloud-business-limits/starcloud-business-limits-biz/src/main/java/com/starcloud/ops/business/limits/convert/userbenefitsusagelog/UserBenefitsUsageLogConvert.java
@@ -1,5 +1,6 @@
 package com.starcloud.ops.business.limits.convert.userbenefitsusagelog;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
@@ -22,6 +23,22 @@ public interface UserBenefitsUsageLogConvert {
     UserBenefitsUsageLogConvert INSTANCE = Mappers.getMapper(UserBenefitsUsageLogConvert.class);
 
     UserBenefitsUsageLogDO convert(UserBenefitsUsageLogCreateReqVO bean);
+
+    default UserBenefitsUsageLogDO InsertConvert(UserBenefitsUsageLogCreateReqVO bean) {
+
+        UserBenefitsUsageLogDO userBenefitsUsageLogDO = new UserBenefitsUsageLogDO();
+        userBenefitsUsageLogDO.setUserId(bean.getUserId());
+        userBenefitsUsageLogDO.setAction(bean.getAction());
+        userBenefitsUsageLogDO.setBenefitsType(bean.getBenefitsType());
+        userBenefitsUsageLogDO.setAmount(bean.getAmount());
+        userBenefitsUsageLogDO.setOutId(bean.getOutId());
+        userBenefitsUsageLogDO.setBenefitsIds(bean.getBenefitsIds());
+        userBenefitsUsageLogDO.setUsageTime(bean.getUsageTime());
+        userBenefitsUsageLogDO.setCreator(bean.getCreator());
+        userBenefitsUsageLogDO.setUpdater(bean.getUpdater());
+        userBenefitsUsageLogDO.setTenantId(bean.getTenantId());
+        return userBenefitsUsageLogDO;
+    }
 
     UserBenefitsUsageLogDO convert(UserBenefitsUsageLogUpdateReqVO bean);
 
