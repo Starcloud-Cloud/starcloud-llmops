42cb1ab00991e58f045f4ac12c4e9ba30d523be1
df007df <duanfeisj@gmail.com>
2023-09-27 14:02:27 +0800

    fix

--- starcloud-llmops/starcloud-llm/starcloud-langchain/src/main/java/com/starcloud/ops/llm/langchain/core/memory/BaseChatMemory.java
+++ starcloud-llmops/starcloud-llm/starcloud-langchain/src/main/java/com/starcloud/ops/llm/langchain/core/memory/BaseChatMemory.java
@@ -5,6 +5,7 @@ import com.starcloud.ops.llm.langchain.core.prompt.base.variable.BaseVariable;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,5 +37,12 @@ public abstract class BaseChatMemory extends BaseMemory<BaseLLMResult> {
         getChatHistory().addUserMessage(String.valueOf(variable.getValue()));
         getChatHistory().addAiMessage(result.getText());
     }
+
+
+    public void saveContext(BaseVariable input, BaseVariable output) {
+
+        this.saveContext(Arrays.asList(input), BaseLLMResult.data(String.valueOf(output.getValue())));
+    }
+
 
 }
