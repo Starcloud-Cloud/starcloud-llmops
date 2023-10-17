e8e759371a5a7dd553be8f6ac4ad632f868cd312
17835411844@163.com <17835411844@163.com>
2023-10-17 16:38:44 +0800

    新建Model listing

---
+++ starcloud-llmops/starcloud-business/starcloud-business-listing/starcloud-business-listing-biz/src/main/java/com/starcloud/ops/business/listing/service/sellersprite/SellerSpriteServiceImpl.java
@@ -0,0 +1,70 @@
+package com.starcloud.ops.business.listing.service.sellersprite;
+
+import com.starcloud.ops.business.listing.service.sellersprite.DTO.request.GetPrepareDTO;
+import org.springframework.stereotype.Service;
+
+/**
+ * 卖家精灵实现类
+ */
+
+@Service
+public class SellerSpriteServiceImpl implements SellerSpriteService {
+
+    /**
+     * 获取可查询时间
+     */
+    @Override
+    public void getDateList() {
+
+    }
+
+    /**
+     * 关键词挖掘- 根据关键词获取数据
+     */
+    @Override
+    public void keywordMiner() {
+
+    }
+
+    /**
+     * 关键词反查
+     */
+    @Override
+    public void keywordReversing() {
+
+    }
+
+    /**
+     * 根据 ASIN 获取变种
+     *
+     * @param getPrepareDTO
+     */
+    @Override
+    public void extendPrepare(GetPrepareDTO getPrepareDTO) {
+
+    }
+
+    /**
+     * 根据 ASIN 拓展流量词
+     */
+    @Override
+    public void extendAsin() {
+
+    }
+
+    /**
+     * 根据 ASIN 获取 Listing
+     */
+    @Override
+    public void getListingByAsin() {
+
+    }
+
+    /**
+     * 品牌检测
+     */
+    @Override
+    public void checkBrand() {
+
+    }
+}
