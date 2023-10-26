b0419ee943b0f615d13379946a9f7b008292bdf6
17835411844@163.com <17835411844@163.com>
2023-10-26 11:43:41 +0800

    关键词原数据

--- starcloud-llmops/starcloud-business/starcloud-business-limits/starcloud-business-limits-api/src/main/java/com/starcloud/ops/business/limits/enums/ProductEnum.java
+++ starcloud-llmops/starcloud-business/starcloud-business-limits/starcloud-business-limits-api/src/main/java/com/starcloud/ops/business/limits/enums/ProductEnum.java
@@ -26,7 +26,7 @@ public enum ProductEnum {
     AI_PRO_MONTH("pro_month", "魔法ai-团队版-月付", 49900, "团队版-月付", "https://cn-test.llmops-ui-user.hotsalestar.com/static/media/user-round.13b5a31bebd2cc6016d6db2cac8e92d1.svg",
             UserLevelEnums.PRO, ProductTimeEnum.MONTH, BenefitsStrategyTypeEnums.PAY_PRO_MONTH),
 
-    AI_BASIC_YEAR("basic_year", "魔法ai-基础版-月付", 59900, "基础版-月付", "https://cn-test.llmops-ui-user.hotsalestar.com/static/media/user-round.13b5a31bebd2cc6016d6db2cac8e92d1.svg",
+    AI_BASIC_YEAR("basic_year", "魔法ai-基础版-年付", 59900, "基础版-年付", "https://cn-test.llmops-ui-user.hotsalestar.com/static/media/user-round.13b5a31bebd2cc6016d6db2cac8e92d1.svg",
             UserLevelEnums.BASIC, ProductTimeEnum.MONTH, BenefitsStrategyTypeEnums.PAY_BASIC_YEAR),
     AI_PLUS_YEAR("plus_year", "魔法ai-高级版-年付", 199900, "高级版-年付", "https://cn-test.llmops-ui-user.hotsalestar.com/static/media/user-round.13b5a31bebd2cc6016d6db2cac8e92d1.svg",
             UserLevelEnums.PLUS, ProductTimeEnum.YEAR, BenefitsStrategyTypeEnums.PAY_PLUS_YEAR),
