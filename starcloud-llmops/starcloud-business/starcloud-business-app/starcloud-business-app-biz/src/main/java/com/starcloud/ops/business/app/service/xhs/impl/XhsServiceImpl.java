bfa97e0ea43596cd69b83868b4b4246c1e85969f
nacoyer <nacoyer@qq.com>
2023-11-02 15:30:39 +0800

    xhs

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/service/xhs/impl/XhsServiceImpl.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/service/xhs/impl/XhsServiceImpl.java
@@ -2,6 +2,8 @@ package com.starcloud.ops.business.app.service.xhs.impl;
 
 import cn.iocoder.yudao.framework.common.exception.ErrorCode;
 import cn.iocoder.yudao.framework.common.exception.util.ServiceExceptionUtil;
+import com.google.common.collect.Lists;
+import com.starcloud.ops.business.app.api.app.vo.request.AppReqVO;
 import com.starcloud.ops.business.app.api.app.vo.response.config.WorkflowConfigRespVO;
 import com.starcloud.ops.business.app.api.app.vo.response.config.WorkflowStepWrapperRespVO;
 import com.starcloud.ops.business.app.api.app.vo.response.variable.VariableItemRespVO;
@@ -10,15 +12,28 @@ import com.starcloud.ops.business.app.api.market.vo.response.AppMarketRespVO;
 import com.starcloud.ops.business.app.api.xhs.XhsAppResponse;
 import com.starcloud.ops.business.app.api.xhs.XhsExecuteRequest;
 import com.starcloud.ops.business.app.api.xhs.XhsExecuteResponse;
+import com.starcloud.ops.business.app.controller.admin.app.vo.AppExecuteReqVO;
+import com.starcloud.ops.business.app.controller.admin.app.vo.AppExecuteRespVO;
+import com.starcloud.ops.business.app.convert.market.AppMarketConvert;
+import com.starcloud.ops.business.app.domain.entity.workflow.ActionResponse;
+import com.starcloud.ops.business.app.enums.app.AppModelEnum;
+import com.starcloud.ops.business.app.enums.app.AppSceneEnum;
 import com.starcloud.ops.business.app.service.app.AppService;
 import com.starcloud.ops.business.app.service.market.AppMarketService;
 import com.starcloud.ops.business.app.service.xhs.XhsService;
+import com.starcloud.ops.business.app.validate.AppValidate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+
+import static com.starcloud.ops.business.app.enums.ErrorCodeConstants.EXECUTE_LISTING_CONFIG_FAILURE;
+import static com.starcloud.ops.business.app.enums.ErrorCodeConstants.EXECUTE_LISTING_STEP_FAILURE;
+import static com.starcloud.ops.business.app.enums.ErrorCodeConstants.EXECUTE_LISTING_VARIABLE_FAILURE;
 
 /**
  * @author nacoyer
@@ -74,4 +89,96 @@ public class XhsServiceImpl implements XhsService {
 
         return null;
     }
+
+    /**
+     * 执行应用
+     *
+     * @param uid       应用UID
+     * @param appParams 应用参数
+     * @return 执行结果
+     */
+    private String execute(String uid, Map<String, Object> appParams) {
+        AppMarketRespVO appMarket = appMarketService.get(uid);
+        AppExecuteReqVO executeRequest = buildExecuteRequest(appMarket, appParams);
+        AppExecuteRespVO executeResponse = appService.execute(executeRequest);
+        if (Objects.isNull(executeResponse)) {
+            return "";
+        }
+        if (!executeResponse.getSuccess()) {
+            return "";
+        }
+        if (Objects.isNull(executeResponse.getResult())) {
+            return "";
+        }
+        if (!(executeResponse.getResult() instanceof ActionResponse)) {
+            return "";
+        }
+
+        ActionResponse result = (ActionResponse) executeResponse.getResult();
+        if (!result.getSuccess()) {
+            return "";
+        }
+
+        return result.getAnswer();
+    }
+
+    /**
+     * 构建执行请求
+     *
+     * @param app       应用
+     * @param appParams 请求
+     * @return 执行请求
+     */
+    private AppExecuteReqVO buildExecuteRequest(AppMarketRespVO app, Map<String, Object> appParams) {
+        AppExecuteReqVO executeRequest = new AppExecuteReqVO();
+        executeRequest.setMode(AppModelEnum.COMPLETION.name());
+        executeRequest.setScene(AppSceneEnum.LISTING_GENERATE.name());
+        executeRequest.setAppUid(app.getUid());
+        executeRequest.setAppReqVO(transform(app, appParams));
+        return executeRequest;
+    }
+
+    /**
+     * 转换请求，转换为应用请求，并且填充参数
+     *
+     * @param app       应用
+     * @param appParams 请求
+     * @return 应用请求
+     */
+    private AppReqVO transform(AppMarketRespVO app, Map<String, Object> appParams) {
+        WorkflowConfigRespVO config = app.getWorkflowConfig();
+        AppValidate.notNull(config, EXECUTE_LISTING_CONFIG_FAILURE);
+
+        List<WorkflowStepWrapperRespVO> steps = config.getSteps();
+        AppValidate.notEmpty(steps, EXECUTE_LISTING_STEP_FAILURE);
+
+        // 直接取第一个步骤，执行第一步骤
+        WorkflowStepWrapperRespVO stepWrapper = steps.get(0);
+        AppValidate.notNull(stepWrapper, EXECUTE_LISTING_STEP_FAILURE);
+
+        VariableRespVO variable = stepWrapper.getVariable();
+        AppValidate.notNull(variable, EXECUTE_LISTING_VARIABLE_FAILURE);
+
+        List<VariableItemRespVO> variables = variable.getVariables();
+        AppValidate.notEmpty(variables, EXECUTE_LISTING_VARIABLE_FAILURE);
+
+        List<VariableItemRespVO> fillVariables = Lists.newArrayList();
+        // 填充变量
+        for (VariableItemRespVO variableItem : variables) {
+            if (appParams.containsKey(variableItem.getField())) {
+                variableItem.setValue(appParams.get(variableItem.getField()));
+                variableItem.setDefaultValue(appParams.get(variableItem.getField()));
+            } else {
+                variableItem.setValue(null);
+            }
+            fillVariables.add(variableItem);
+        }
+        variable.setVariables(fillVariables);
+        stepWrapper.setVariable(variable);
+        steps.set(0, stepWrapper);
+        config.setSteps(steps);
+        app.setWorkflowConfig(config);
+        return AppMarketConvert.INSTANCE.convert(app);
+    }
+
 }
