8c95d1fc67f7a55c3986ca0ac8e38a24aff2736f
fanmiao <aiden_fzq@163.com>
2023-11-09 11:56:08 +0800

    xhs

---
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/xhs/vo/XhsAppBatchExecuteRequest.java
@@ -0,0 +1,14 @@
+package com.starcloud.ops.business.app.controller.admin.xhs.vo;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class XhsAppBatchExecuteRequest extends XhsAppExecuteRequest{
+
+    /**
+     * 任务uid
+     */
+    private String uid;
+}
