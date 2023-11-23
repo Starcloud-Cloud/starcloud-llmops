c7ade985e6ab5864870120e990a4798c3f8a6666
17835411844@163.com <17835411844@163.com>
2023-11-23 16:02:00 +0800

    【ADD】新增优惠券类型 49.1¥

--- starcloud-llmops/starcloud-business/starcloud-business-limits/starcloud-business-limits-api/src/main/java/com/starcloud/ops/business/limits/enums/ProductEnum.java
+++ starcloud-llmops/starcloud-business/starcloud-business-limits/starcloud-business-limits-api/src/main/java/com/starcloud/ops/business/limits/enums/ProductEnum.java
@@ -21,7 +21,7 @@ public enum ProductEnum {
 
 
     AI_BASIC_MONTH("basic_month", "魔法ai-基础版-月付", 5900, "基础版-月付", "https://cn-test.llmops-ui-user.hotsalestar.com/static/media/user-round.13b5a31bebd2cc6016d6db2cac8e92d1.svg",
-            UserLevelEnums.BASIC, ProductTimeEnum.MONTH, BenefitsStrategyTypeEnums.PAY_BASIC_MONTH, new BenefitsStrategyTypeEnums[]{BenefitsStrategyTypeEnums.DIRECT_DISCOUNT_10}, ProductSignEnum.PRODUCT_BASIC_CONFIG),
+            UserLevelEnums.BASIC, ProductTimeEnum.MONTH, BenefitsStrategyTypeEnums.PAY_BASIC_MONTH, new BenefitsStrategyTypeEnums[]{BenefitsStrategyTypeEnums.DIRECT_DISCOUNT_10,BenefitsStrategyTypeEnums.DIRECT_DISCOUNT_49}, ProductSignEnum.PRODUCT_BASIC_CONFIG),
     AI_PLUS_MONTH("plus_month", "魔法ai-高级版-月付", 19900, "高级版-月付", "https://cn-test.llmops-ui-user.hotsalestar.com/static/media/user-round.13b5a31bebd2cc6016d6db2cac8e92d1.svg",
             UserLevelEnums.PLUS, ProductTimeEnum.MONTH, BenefitsStrategyTypeEnums.PAY_PLUS_MONTH, new BenefitsStrategyTypeEnums[]{BenefitsStrategyTypeEnums.DIRECT_DISCOUNT_10}, null),
     AI_PRO_MONTH("pro_month", "魔法ai-团队版-月付", 49900, "团队版-月付", "https://cn-test.llmops-ui-user.hotsalestar.com/static/media/user-round.13b5a31bebd2cc6016d6db2cac8e92d1.svg",
