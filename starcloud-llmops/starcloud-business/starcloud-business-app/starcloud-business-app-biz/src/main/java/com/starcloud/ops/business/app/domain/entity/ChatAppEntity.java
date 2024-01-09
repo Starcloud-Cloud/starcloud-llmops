027290f4c5d79afad7159294c1790c17aed934a9
fanmiao <aiden.fzq@gmail.com>
2024-01-09 17:24:02 +0800

    fix: chat error

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/domain/entity/ChatAppEntity.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/domain/entity/ChatAppEntity.java
@@ -222,7 +222,8 @@ public class ChatAppEntity<Q, R> extends BaseAppEntity<ChatRequestVO, JsonData> 
         } catch (ServiceException exception) {
             log.error("聊天执行异常(ServerException): 错误信息: {}", exception.getMessage());
             // ServiceException 时候将消息UID传入exception中
-            exception.setBizUid(request.getScene() + "_" + request.getAppUid());
+            exception.setBizUid(request.getAppUid());
+            exception.setScene(request.getScene());
             throw exception;
         }
 
