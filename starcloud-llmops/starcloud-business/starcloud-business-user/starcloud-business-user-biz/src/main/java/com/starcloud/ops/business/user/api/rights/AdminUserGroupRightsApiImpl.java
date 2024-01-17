4aa9793478429a5bda888aca1702388377bdb5a0
fanmiao <aiden.fzq@gmail.com>
2024-01-17 09:45:40 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-user/starcloud-business-user-biz/src/main/java/com/starcloud/ops/business/user/api/rights/AdminUserGroupRightsApiImpl.java
+++ starcloud-llmops/starcloud-business/starcloud-business-user/starcloud-business-user-biz/src/main/java/com/starcloud/ops/business/user/api/rights/AdminUserGroupRightsApiImpl.java
@@ -11,8 +11,10 @@ import cn.iocoder.yudao.module.system.dal.dataobject.dept.DeptDO;
 import cn.iocoder.yudao.module.system.enums.common.TimeRangeTypeEnum;
 import cn.iocoder.yudao.module.system.service.dept.DeptService;
 import cn.iocoder.yudao.module.system.service.user.AdminUserService;
+import com.starcloud.ops.business.user.dal.dataObject.dept.UserDeptDO;
 import com.starcloud.ops.business.user.enums.rights.AdminUserRightsBizTypeEnum;
 import com.starcloud.ops.business.user.enums.rights.AdminUserRightsTypeEnum;
+import com.starcloud.ops.business.user.service.dept.UserDeptService;
 import com.starcloud.ops.business.user.service.rights.AdminUserRightsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,7 @@ import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static cn.iocoder.yudao.framework.common.exception.util.ServiceExceptionUtil.exception;
 import static com.starcloud.ops.business.user.enums.ErrorCodeConstant.RIGHTS_BIZ_NOT_SUPPORT;
@@ -46,11 +49,15 @@ public class AdminUserGroupRightsApiImpl extends AdminUserRightsApiImpl {
     @Autowired
     private AdminUserApi adminUserApi;
 
+    @Resource
+    private UserDeptService userDeptService;
+
     @Override
     public void reduceRights(Long userId, AdminUserRightsTypeEnum rightsType, Integer rightAmount,
                              Integer bizType, String bizId) {
-
-        Long deptUserId = this.getDeptRightsUserId(userId, rightsType, rightAmount);
+        UserDeptDO userDeptDO = this.getDeptRightsUserId(userId, rightsType, rightAmount);
+        userDeptService.recordRights(userDeptDO,rightsType,rightAmount);
+        Long deptUserId = Optional.ofNullable(userDeptDO).map(UserDeptDO::getUserId).orElse(userId);
         super.reduceRights(deptUserId, rightsType, rightAmount, bizType, bizId);
 
     }
@@ -65,90 +72,37 @@ public class AdminUserGroupRightsApiImpl extends AdminUserRightsApiImpl {
      */
     @Override
     public Boolean calculateUserRightsEnough(Long userId, AdminUserRightsTypeEnum rightsType, Integer rightAmount) {
-
-        Long deptUserId = this.getDeptRightsUserId(userId, rightsType, rightAmount);
+        UserDeptDO userDeptDO = this.getDeptRightsUserId(userId, rightsType, rightAmount);
+        Long deptUserId = Optional.ofNullable(userDeptDO).map(UserDeptDO::getUserId).orElse(userId);
         return super.calculateUserRightsEnough(deptUserId, rightsType, rightAmount);
     }
 
 
     /**
-     * 获取应该创作权益的用户
+     * 获取应该创作权益的用户   返回部门超级管理员id
      * 1，获取当前用户的部门
      * 2，判断是否是部门管理员
      * 1）是部门管理员，返回
      * 2）不是部门管理员，优先获取部门管理员。判断管理员有无剩余点数
      * 3，返回有剩余点的用户ID（管理员或当前用户）
      */
-    protected Long getDeptRightsUserId(Long currentUserId, AdminUserRightsTypeEnum rightsType, Integer rightAmount) {
-
-        AdminUserRespDTO adminUserRespDTO = adminUserApi.getUser(currentUserId);
-
-        if (adminUserRespDTO == null) {
-            log.warn("getDeptRightsUserId: {} {}", currentUserId, JSONUtil.toJsonPrettyStr(adminUserRespDTO));
-            return currentUserId;
+    protected UserDeptDO getDeptRightsUserId(Long currentUserId, AdminUserRightsTypeEnum rightsType, Integer rightAmount) {
+        UserDeptDO userDeptDO = userDeptService.selectSuperAdminId(currentUserId);
+        if (userDeptDO == null) {
+            //之前数据没配置, 这里做兼容处理
+            return null;
         } else {
-            log.info("getDeptRightsUserId: {} {}", currentUserId, JSONUtil.toJsonPrettyStr(adminUserRespDTO));
-        }
-
-        //获取当前用户部门ID,只能获取用户当前激活的部门
-        Long deptId = adminUserRespDTO.getDeptId();
-        //找到部门的管理员
-        DeptDO deptDO = deptService.getDept(deptId);
-        if (deptDO != null) {
-            //部门管理员不是当前用户，获取管理员ID
-            if (deptDO.getLeaderUserId() == null) {
-                //之前数据没配置, 这里做兼容处理
-                return currentUserId;
-            } else {
-
-                if (!deptDO.getLeaderUserId().equals(currentUserId)) {
-                    //判断管理员是否还有权益
-                    if (super.calculateUserRightsEnough(deptDO.getLeaderUserId(), rightsType, rightAmount)) {
-                        log.info("权益切换：当前用户[{}]切换到部门[{}]负责人[{}]", currentUserId, deptDO.getName(), deptDO.getLeaderUserId());
-                        return deptDO.getLeaderUserId();
-                    }
+            if (!currentUserId.equals(userDeptDO.getUserId())) {
+                //判断管理员是否还有权益
+                if (super.calculateUserRightsEnough(userDeptDO.getUserId(), rightsType, rightAmount)) {
+                    log.info("权益切换：当前用户[{}]切换到部门负责人[{}]", currentUserId, userDeptDO.getUserId());
+                    return userDeptDO;
                 }
+            } else {
+                return userDeptDO;
             }
         }
-
-//        try {
-//            AdminUserRespDTO adminUserRespDTO = adminUserApi.getUser(currentUserId);
-//
-//            if (adminUserRespDTO == null) {
-//                log.warn("getDeptRightsUserId: {} {}", currentUserId, JSONUtil.toJsonPrettyStr(adminUserRespDTO));
-//                return currentUserId;
-//            } else {
-//                log.info("getDeptRightsUserId: {} {}", currentUserId, JSONUtil.toJsonPrettyStr(adminUserRespDTO));
-//            }
-//
-//            //获取当前用户部门ID,只能获取用户当前激活的部门
-//            Long deptId = adminUserRespDTO.getDeptId();
-//            //找到部门的管理员
-//            DeptDO deptDO = deptService.getDept(deptId);
-//            if (deptDO != null) {
-//                //部门管理员不是当前用户，获取管理员ID
-//                if (deptDO.getLeaderUserId() == null) {
-//                    //之前数据没配置, 这里做兼容处理
-//                    return currentUserId;
-//                } else {
-//
-//                    if (!deptDO.getLeaderUserId().equals(currentUserId)) {
-//                        //判断管理员是否还有权益
-//                        if (super.calculateUserRightsEnough(deptDO.getLeaderUserId(), rightsType, rightAmount)) {
-//                            log.info("权益切换：当前用户[{}]切换到部门[{}]负责人[{}]", currentUserId, deptDO.getName(), deptDO.getLeaderUserId());
-//                            return deptDO.getLeaderUserId();
-//                        }
-//                    }
-//                }
-//            }
-//
-//        } catch (Exception e) {
-//            log.warn("getDeptRightsUserId getUser is fail: {}", e.getMessage(), e);
-//        }
-
-
-        return currentUserId;
-
+        return null;
     }
 
 }
