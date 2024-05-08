fba67f1da3f351feb2fcaf2aa8c8d92cb55fb18e
fanmiao <aidne_fzq@163.com>
2024-05-08 15:14:44 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/enums/xhs/material/MaterialFieldTypeEnum.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/enums/xhs/material/MaterialFieldTypeEnum.java
@@ -1,6 +1,7 @@
 package com.starcloud.ops.business.app.enums.xhs.material;
 
 import com.starcloud.ops.framework.common.api.dto.Option;
+import com.starcloud.ops.framework.common.api.enums.IEnumable;
 import lombok.Getter;
 
 import java.util.Arrays;
@@ -9,39 +10,45 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
-public enum FieldTypeEnum {
+public enum MaterialFieldTypeEnum implements IEnumable<String> {
 
     image("image", "图片"),
 
-    listImage("listImage", "图片"),
-
     string("string", "字符串输入框"),
 
-    select("select", "下拉框"),
-
-    weburl("weburl", "http地址"),
-
-    listStr("listStr","字符串集合"),
-
     textBox("textBox", "字符串文本框"),
+
+    document("document", "文档路径"),
+
+
     ;
 
     private final String typeCode;
 
     private final String desc;
 
-    FieldTypeEnum(String typeCode, String desc) {
+    MaterialFieldTypeEnum(String typeCode, String desc) {
         this.typeCode = typeCode;
         this.desc = desc;
     }
 
     public static List<Option> options() {
-        return Arrays.stream(values()).sorted(Comparator.comparingInt(FieldTypeEnum::ordinal))
+        return Arrays.stream(values()).sorted(Comparator.comparingInt(MaterialFieldTypeEnum::ordinal))
                 .map(item -> {
                     Option option = new Option();
                     option.setLabel(item.getDesc());
                     option.setValue(item.getTypeCode());
                     return option;
                 }).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getCode() {
+        return this.typeCode;
+    }
+
+    @Override
+    public String getLabel() {
+        return this.desc;
     }
 }
