c9a71a0563ad9b3cace829a2543bf6d65d06f23e
nacoyer <nacoyer@qq.com>
2024-05-22 15:50:53 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/service/xhs/manager/CreativeAlarmManager.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/service/xhs/manager/CreativeAlarmManager.java
@@ -1,20 +1,24 @@
-package com.starcloud.ops.business.app.domain.manager;
+package com.starcloud.ops.business.app.service.xhs.manager;
 
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.iocoder.yudao.framework.common.exception.ServiceException;
-import cn.iocoder.yudao.framework.common.util.json.JsonUtils;
 import cn.iocoder.yudao.module.system.api.sms.SmsSendApi;
 import cn.iocoder.yudao.module.system.api.sms.dto.send.SmsSendSingleToUserReqDTO;
-import com.starcloud.ops.business.app.api.AppValidate;
 import com.starcloud.ops.business.app.api.app.vo.request.AppContextReqVO;
+import com.starcloud.ops.business.app.api.market.vo.response.AppMarketRespVO;
+import com.starcloud.ops.business.app.api.xhs.content.dto.CreativeContentExecuteParam;
+import com.starcloud.ops.business.app.api.xhs.content.vo.response.CreativeContentRespVO;
 import com.starcloud.ops.business.app.enums.app.AppModelEnum;
 import com.starcloud.ops.business.app.enums.app.AppSceneEnum;
+import com.starcloud.ops.business.app.enums.xhs.content.CreativeContentStatusEnum;
+import com.starcloud.ops.business.app.service.xhs.content.CreativeContentService;
 import com.starcloud.ops.business.app.util.UserUtils;
 import com.starcloud.ops.business.core.config.notice.DingTalkNoticeProperties;
 import com.starcloud.ops.framework.common.api.util.ExceptionUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -24,9 +28,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 import static com.starcloud.ops.business.user.enums.ErrorCodeConstant.USER_RIGHTS_BEAN_NOT_ENOUGH;
 import static com.starcloud.ops.business.user.enums.ErrorCodeConstant.USER_RIGHTS_IMAGE_NOT_ENOUGH;
+import static com.starcloud.ops.business.user.enums.ErrorCodeConstant.USER_RIGHTS_MATRIX_BEAN_NOT_ENOUGH;
+import static com.starcloud.ops.business.user.enums.ErrorCodeConstant.USER_RIGHTS_NOT_ENOUGH;
 
 /**
  * 应用报警管理
@@ -35,11 +42,12 @@ import static com.starcloud.ops.business.user.enums.ErrorCodeConstant.USER_RIGHT
  * @version 1.0.0
  * @since 2021-06-22
  */
+@SuppressWarnings("all")
 @Slf4j
 @Component
-public class AppAlarmManager {
+public class CreativeAlarmManager {
 
-    private static final String TEMPLATE_CODE = "APP_EXECUTE_ALARM_TEMPLATE";
+    private static final String TEMPLATE_CODE = "CREATIVE_EXECUTE_ALARM_TEMPLATE";
 
     @Resource
     private SmsSendApi smsSendApi;
@@ -47,15 +55,35 @@ public class AppAlarmManager {
     @Resource
     private DingTalkNoticeProperties dingTalkNoticeProperties;
 
+    @Resource
+    private CreativeContentService creativeContentService;
+
     /**
      * 向钉钉群发送报警信息
      * <p>
      */
-    public void executeAlarm(AppContextReqVO request, Throwable throwable) {
-        log.info("应用执行失败：开始发送应用执行报警信息");
+    public void executeAlarm(String contentUid, Boolean force, Integer maxRetry, Throwable throwable) {
+        log.info("创作内容执行失败：开始发送创作内容执行报警信息");
         try {
+            // 查询创作内容详情
+            CreativeContentRespVO content = creativeContentService.get(contentUid);
+            if (Objects.isNull(content)) {
+                log.error("发送创作内容执行报警信息失败：创作内容不存在：{}", contentUid);
+                return;
+            }
+
+            // 如果不是强制执行，则判断是否需要发送报警信息
+            if (!force) {
+                // 如果创作内容不是最终失败，或者重试次数小于最大重试次数，不发送报警信息
+                if (!CreativeContentStatusEnum.ULTIMATE_FAILURE.name().equals(content.getStatus()) ||
+                        (content.getRetryCount() == null || content.getRetryCount() < maxRetry)) {
+                    log.info("创作内容执行失败：创作内容不是最终失败，或者重试次数小于最大重试次数，不发送报警信息");
+                    return;
+                }
+            }
+
             // 构建模板信息
-            Map<String, Object> templateParams = this.buildTemplateParams(request, throwable);
+            Map<String, Object> templateParams = this.buildTemplateParams(content, throwable);
             // 构建发送的请求
             SmsSendSingleToUserReqDTO messageRequest = new SmsSendSingleToUserReqDTO();
             messageRequest.setUserId(1L);
@@ -64,9 +92,9 @@ public class AppAlarmManager {
             messageRequest.setTemplateParams(templateParams);
             // 发送告警信息
             smsSendApi.sendSingleSmsToAdmin(messageRequest);
-            log.info("应用执行失败: 开始发送应用执行报警信息成功");
+            log.info("创作内容执行失败: 开始发送创作内容执行报警信息成功");
         } catch (Exception exception) {
-            log.error("应用执行失败: 发送应用执行报警信息失败", exception);
+            log.error("创作内容执行失败: 发送创作内容执行报警信息失败", exception);
             // ignore
             // 发送失败，不抛出异常，避免影响业务
         }
@@ -74,7 +102,10 @@ public class AppAlarmManager {
 
     /**
      * 模板：
-     * ####【应用执行告警-{environment}】
+     * ####【创作内容执行告警-{environment}】
+     * > - 内容UID: {contentUid}
+     * > - 批次UID: {batchUid}
+     * > - 计划UID: {planUid}
      * > - 应用UID: {appUid}
      * > - 执行用户: {nickname}(userId)
      * > - 用户等级: {userLevel}
@@ -86,34 +117,42 @@ public class AppAlarmManager {
      * > - 异常原因：{stackTrace}
      * > - 扩展信息: {extended}
      *
-     * @param request   请求
+     * @param content   请求
      * @param throwable 异常
      * @return 模板参数
      */
-    private Map<String, Object> buildTemplateParams(AppContextReqVO request, Throwable throwable) {
+    private Map<String, Object> buildTemplateParams(CreativeContentRespVO content, Throwable throwable) {
 
-        // 应用模式
-        AppModelEnum appModelEnum = AppModelEnum.getByName(request.getMode());
-        AppValidate.notNull(appModelEnum, "应用模式不存在");
-        String appModeLabel = Objects.nonNull(appModelEnum) ? appModelEnum.getLabel() : StrUtil.EMPTY;
-
-        // 执行场景
-        AppSceneEnum appSceneEnum = AppSceneEnum.getByName(request.getScene());
-        AppValidate.notNull(appSceneEnum, "应用场景不存在");
-        String appSceneLabel = Objects.nonNull(appSceneEnum) ? appSceneEnum.getLabel() : StrUtil.EMPTY;
+        // 获取应用信息
+        AppMarketRespVO appInformation = Optional.ofNullable(content)
+                .map(CreativeContentRespVO::getExecuteParam)
+                .map(CreativeContentExecuteParam::getAppInformation)
+                .orElseThrow(() -> new RuntimeException("创作内容执行应用参数为空!"));
 
         // 系统环境
         String environment = this.getEnvironment();
-        // 应用 UID
-        String appUid = request.getAppUid();
-        // 应用名称
-        String appName = request.getAppName();
+        // 内容UID
+        String contentUid = content.getUid();
+        // 批次UID
+        String batchUid = content.getBatchUid();
+        // 计划UID
+        String planUid = content.getPlanUid();
+        // 应用UID
+        String appUid = appInformation.getUid();
+
         // 用户ID
-        String userId = this.getUserId(request);
+        String userId = content.getCreator();
         // 用户昵称
-        String nickname = this.getNickname(request);
+        String nickname = this.getNickname(userId);
         // 用户等级
-        String userLevel = this.getUserLevel(request);
+        String userLevel = this.getUserLevel(userId);
+
+        // 执行场景固定位小红书模式
+        String appSceneLabel = AppSceneEnum.XHS_WRITING.getLabel();
+        // 应用名称
+        String appName = appInformation.getName();
+        // 应用模式固定位生成模式
+        String appModeLabel = AppModelEnum.COMPLETION.getLabel();
         // 通知时间
         String notifyTime = LocalDateTimeUtil.formatNormal(LocalDateTime.now());
 
@@ -129,9 +168,11 @@ public class AppAlarmManager {
                 } else {
                     serviceException = (ServiceException) throwable.getCause();
                 }
-                // 魔法豆/图片不足，不打印堆栈信息
+                // 魔法豆/图片不足/矩阵豆，不打印堆栈信息
                 if (USER_RIGHTS_BEAN_NOT_ENOUGH.getCode().equals(serviceException.getCode()) ||
-                        USER_RIGHTS_IMAGE_NOT_ENOUGH.getCode().equals(serviceException.getCode())) {
+                        USER_RIGHTS_IMAGE_NOT_ENOUGH.getCode().equals(serviceException.getCode()) ||
+                        USER_RIGHTS_NOT_ENOUGH.getCode().equals(serviceException.getCode()) ||
+                        USER_RIGHTS_MATRIX_BEAN_NOT_ENOUGH.getCode().equals(serviceException.getCode())) {
                     stackTrace = "";
                 }
             } else {
@@ -140,11 +181,14 @@ public class AppAlarmManager {
         }
 
         // 扩展信息
-        String extended = this.getExtended(request);
+        String extended = this.getExtended(content);
 
         // 构建发送模板信息
         Map<String, Object> templateParams = new HashMap<>();
         templateParams.put("environment", environment);
+        templateParams.put("contentUid", contentUid);
+        templateParams.put("batchUid", batchUid);
+        templateParams.put("planUid", planUid);
         templateParams.put("appUid", appUid);
         templateParams.put("nickname", nickname);
         templateParams.put("userId", userId);
@@ -187,28 +231,28 @@ public class AppAlarmManager {
      * 获取用户昵称
      * 如果游客ID不为空，则返回游客昵称，否则返回用户昵称
      *
-     * @param request 请求
+     * @param userId 用户ID
      * @return 用户昵称
      */
-    private String getNickname(AppContextReqVO request) {
-        if (Objects.nonNull(request.getEndUserId())) {
-            return "游客";
+    private String getNickname(String userId) {
+        if (StringUtils.isBlank(userId)) {
+            throw new RuntimeException("用户ID为空");
         }
-        return UserUtils.getUsername(request.getUserId());
+        return UserUtils.getUsername(userId);
     }
 
     /**
      * 获取用户等级
      *
-     * @param request 请求
+     * @param userId 请求
      * @return 用户等级
      */
-    private String getUserLevel(AppContextReqVO request) {
-        if (Objects.nonNull(request.getEndUserId())) {
-            return "";
+    private String getUserLevel(String userId) {
+        if (StringUtils.isBlank(userId)) {
+            throw new RuntimeException("用户ID为空");
         }
-        Map<Long, List<String>> longListMap = UserUtils.mapUserRoleName(Collections.singletonList(request.getUserId()));
-        List<String> roleNames = longListMap.get(request.getUserId());
+        Map<Long, List<String>> longListMap = UserUtils.mapUserRoleName(Collections.singletonList(Long.valueOf(userId)));
+        List<String> roleNames = longListMap.get(Long.valueOf(userId));
         if (CollectionUtil.isEmpty(roleNames)) {
             return "";
         }
@@ -228,16 +272,11 @@ public class AppAlarmManager {
     /**
      * 获取扩展信息
      *
-     * @param request 请求
+     * @param content 创作内容
      * @return 扩展信息
      */
-    private String getExtended(AppContextReqVO request) {
-        Map<String, Object> extended = request.getExtended();
-        if (CollectionUtil.isEmpty(extended)) {
-            return "";
-        }
-        extended.remove("isSendAlarm");
-        return JsonUtils.toJsonPrettyString(extended);
+    private String getExtended(CreativeContentRespVO content) {
+        return "";
     }
 
 }
