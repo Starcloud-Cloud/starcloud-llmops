d682696ef36b68941d6bdffec71ba7f7f08bbfc4
fanmiao <aidne_fzq@163.com>
2024-06-26 15:12:36 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/domain/entity/BaseAppEntity.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/domain/entity/BaseAppEntity.java
@@ -477,7 +477,12 @@ public abstract class BaseAppEntity<Q extends AppContextReqVO, R> {
     @JSONField(serialize = false)
     public void update() {
         this.validate(null);
-        disposeMaterial();
+        try {
+            disposeMaterial();
+        } catch (Exception e) {
+            log.warn("disposeMaterial error", e);
+        }
+
         this.doUpdate();
     }
 
