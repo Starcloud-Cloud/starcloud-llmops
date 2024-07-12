80595e425c2596e2c37d5ca47aecab76570390c9
17835411844@163.com <17835411844@163.com>
2024-07-12 17:15:32 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/materiallibrary/vo/library/MaterialLibrarySavePlugInConfigReqVO.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/materiallibrary/vo/library/MaterialLibrarySavePlugInConfigReqVO.java
@@ -3,11 +3,14 @@ package com.starcloud.ops.business.app.controller.admin.materiallibrary.vo.libra
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Schema(description = "管理后台 - 素材知识库新增/修改 Request VO")
 @Data
 public class MaterialLibrarySavePlugInConfigReqVO {
 
     @Schema(description = "编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "26459")
+    @NotNull(message = "编号ID不能为空")
     private Long id;
 
     @Schema(description = "插件配置", requiredMode = Schema.RequiredMode.REQUIRED, example = "2")
