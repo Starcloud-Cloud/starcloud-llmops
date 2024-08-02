89a28e1ccd80e7103fd214a19d05a76398a557f1
nacoyer <nacoyer@qq.com>
2024-08-02 13:44:00 +0800

    fix

---
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/validation/ValidationMessage.java
@@ -0,0 +1,18 @@
+package com.starcloud.ops.business.app.validation;
+
+import lombok.Data;
+
+/**
+ * 校验消息，用于校验失败时返回给前端的消息
+ *
+ * @author nacoyer
+ * @date 2021-06-22
+ * @since 1.0.0
+ */
+@Data
+public class ValidationMessage implements java.io.Serializable {
+
+    private static final long serialVersionUID = -9151872198537474550L;
+
+
+}
