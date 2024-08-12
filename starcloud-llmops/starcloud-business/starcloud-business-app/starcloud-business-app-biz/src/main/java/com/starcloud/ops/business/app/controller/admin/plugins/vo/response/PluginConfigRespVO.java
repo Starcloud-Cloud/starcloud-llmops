e11d1175f22852f2804c641b50f1619a5fbae076
fanmiao <aidne_fzq@163.com>
2024-08-12 11:32:38 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/plugins/vo/response/PluginConfigRespVO.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/plugins/vo/response/PluginConfigRespVO.java
@@ -3,8 +3,20 @@ package com.starcloud.ops.business.app.controller.admin.plugins.vo.response;
 import com.starcloud.ops.business.app.controller.admin.plugins.vo.PluginConfigVO;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class PluginConfigRespVO extends PluginConfigVO {
 
     private String uid;
+
+    private Long id;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    private String creator;
+
+    private String updater;
 }
