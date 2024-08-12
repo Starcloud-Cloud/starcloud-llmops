e44f2ce021f49f65197061eafea817237829e838
17835411844@163.com <17835411844@163.com>
2024-08-12 10:27:33 +0800

    fix

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/service/social/SocialUserService.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/service/social/SocialUserService.java
@@ -40,14 +40,12 @@ public interface SocialUserService {
     /**
      * 取消绑定社交用户
      *
-     * @param userId 用户编号
-     * @param userType 全局用户类型
+     * @param userId     用户编号
+     * @param userType   全局用户类型
      * @param socialType 社交平台的类型 {@link SocialTypeEnum}
-     * @param openid 社交平台的 openid
+     * @param openid     社交平台的 openid
      */
     void unbindSocialUser(Long userId, Integer userType, Integer socialType, String openid);
-
-
 
 
     /**
@@ -60,13 +58,13 @@ public interface SocialUserService {
 
     /**
      * 获得社交用户
-     *
+     * <p>
      * 在认证信息不正确的情况下，也会抛出 {@link ServiceException} 业务异常
      *
-     * @param userType 用户类型
+     * @param userType   用户类型
      * @param socialType 社交平台的类型
-     * @param code 授权码
-     * @param state state
+     * @param code       授权码
+     * @param state      state
      * @return 社交用户
      */
     SocialUserRespDTO getSocialUser(Integer userType, Integer socialType, String code, String state);
@@ -89,6 +87,7 @@ public interface SocialUserService {
      * @return 社交用户
      */
     SocialUserDO getNewSocialUser(Long id);
+
     /**
      * 获得社交用户分页
      *
@@ -99,6 +98,7 @@ public interface SocialUserService {
 
     /**
      * 绑定公共号用户
+     *
      * @param socialUserDO
      * @param socialUserBindDO
      */
@@ -106,6 +106,7 @@ public interface SocialUserService {
 
     /**
      * 获得指定用户的社交用户
+     *
      * @param userId
      * @param userType
      * @return
@@ -114,12 +115,13 @@ public interface SocialUserService {
 
     /**
      * 获得指定用户的社交用户
-     * @param userId
-     * @param userType
-     * @return
+     *
+     * @param userId     用户编号
+     * @param userType   用户类型
+     * @param socialType 社交平台的类型
+     * @return 社交用户
      */
-    SocialUserDO getSocialUser(Long userId, Integer userType,Integer socialType );
-
+    SocialUserDO getSocialUser(Long userId, Integer userType, Integer socialType);
 
 
 }
