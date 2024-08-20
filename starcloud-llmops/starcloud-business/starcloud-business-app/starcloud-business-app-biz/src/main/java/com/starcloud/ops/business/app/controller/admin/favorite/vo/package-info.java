008afad3225685688e386abf6b6f85d92137ec66
nacoyer <nacoyer@qq.com>
2024-08-20 15:14:43 +0800

    fix

---
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/favorite/vo/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author nacoyer
+ * @version 1.0.0
+ * @since 2023-06-26
+ */
+package com.starcloud.ops.business.app.controller.admin.favorite.vo;
---
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/xhs/scheme/vo/response/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author nacoyer
+ * @date 2021-06-22
+ * @since 1.0.0
+ */
+package com.starcloud.ops.business.app.controller.admin.xhs.scheme.vo.response;
