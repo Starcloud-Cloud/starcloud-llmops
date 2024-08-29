1028fd02cd0be84b6d6063730fa966928ed3b5db
17835411844@163.com <17835411844@163.com>
2024-08-29 17:21:23 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/service/materiallibrary/impl/MaterialLibraryTableColumnServiceImpl.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/service/materiallibrary/impl/MaterialLibraryTableColumnServiceImpl.java
@@ -99,7 +99,7 @@ public class MaterialLibraryTableColumnServiceImpl implements MaterialLibraryTab
         // 校验名称
         validateMaterialLibraryTableColumnName(updateReqVO.getLibraryId(), updateReqVO.getId(), Collections.singletonList(updateReqVO.getColumnName()));
         // 填充ColumnCode
-        generateColumnCode(Collections.singletonList(updateReqVO));
+        // generateColumnCode(Collections.singletonList(updateReqVO));
 
 
         // 更新
