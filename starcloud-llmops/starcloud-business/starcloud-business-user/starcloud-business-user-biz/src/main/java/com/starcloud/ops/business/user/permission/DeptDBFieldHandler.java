0b99f26e8ba86a23a71f1aafdec604f3e3eedc43
fanmiao <aidne_fzq@163.com>
2024-10-10 15:28:33 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-user/starcloud-business-user-biz/src/main/java/com/starcloud/ops/business/user/permission/DeptDBFieldHandler.java
+++ starcloud-llmops/starcloud-business/starcloud-business-user/starcloud-business-user-biz/src/main/java/com/starcloud/ops/business/user/permission/DeptDBFieldHandler.java
@@ -48,7 +48,7 @@ public class DeptDBFieldHandler extends DefaultDBFieldHandler {
         }
 
         Long userId = null;
-        if (Objects.isNull(loginUser) && Objects.isNull(loginUser.getId())) {
+        if (Objects.isNull(loginUser) || Objects.isNull(loginUser.getId())) {
             userId = UserContextHolder.getUserId();
         } else {
             userId = loginUser.getId();
