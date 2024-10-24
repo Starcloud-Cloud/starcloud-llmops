63b05d3be79919291cc080b41485edefc114a81b
fanmiao <aidne_fzq@163.com>
2024-10-24 15:42:15 +0800

    fix:job log

--- starcloud-llmops/starcloud-business/starcloud-business-job/starcloud-business-job-biz/src/main/java/com/starcloud/ops/business/job/biz/dal/mysql/BusinessJobLogMapper.java
+++ starcloud-llmops/starcloud-business/starcloud-business-job/starcloud-business-job-biz/src/main/java/com/starcloud/ops/business/job/biz/dal/mysql/BusinessJobLogMapper.java
@@ -8,7 +8,9 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.starcloud.ops.business.job.biz.controller.admin.vo.request.JobLogPageReqVO;
 import com.starcloud.ops.business.job.biz.controller.admin.vo.request.LibraryJobLogPageReqVO;
 import com.starcloud.ops.business.job.biz.dal.dataobject.BusinessJobLogDO;
+import com.starcloud.ops.business.job.biz.dal.dataobject.JobLogDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -19,16 +21,18 @@ public interface BusinessJobLogMapper extends BaseMapperX<BusinessJobLogDO> {
     default PageResult<BusinessJobLogDO> page(JobLogPageReqVO pageReqVO) {
         LambdaQueryWrapper<BusinessJobLogDO> wrapper = Wrappers.lambdaQuery(BusinessJobLogDO.class)
                 .eq(BusinessJobLogDO::getBusinessJobUid, pageReqVO.getBusinessJobUid())
-                .orderByDesc(BusinessJobLogDO::getCreateTime)
-                ;
-        return selectPage(pageReqVO,wrapper);
+                .orderByDesc(BusinessJobLogDO::getCreateTime);
+        return selectPage(pageReqVO, wrapper);
     }
 
     default PageResult<BusinessJobLogDO> libraryPage(List<String> JobUidList, PageParam pageReqVO) {
         LambdaQueryWrapper<BusinessJobLogDO> wrapper = Wrappers.lambdaQuery(BusinessJobLogDO.class)
                 .in(BusinessJobLogDO::getBusinessJobUid, JobUidList)
-                .orderByDesc(BusinessJobLogDO::getCreateTime)
-                ;
-        return selectPage(pageReqVO,wrapper);
+                .orderByDesc(BusinessJobLogDO::getCreateTime);
+        return selectPage(pageReqVO, wrapper);
     }
+
+    Long count();
+
+    List<JobLogDTO> pluginLog(@Param("start") Integer start, @Param("size") Integer size);
 }
