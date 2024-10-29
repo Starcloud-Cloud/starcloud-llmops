4df1c96bc31e4fa389a919a5bd1824b09344b86e
fanmiao <aidne_fzq@163.com>
2024-10-29 15:21:35 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/plugins/vo/PluginConfigVO.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-biz/src/main/java/com/starcloud/ops/business/app/controller/admin/plugins/vo/PluginConfigVO.java
@@ -2,6 +2,7 @@ package com.starcloud.ops.business.app.controller.admin.plugins.vo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
@@ -22,6 +23,7 @@ public class PluginConfigVO {
 
     @Schema(description = "绑定名称")
     @NotBlank(message = "绑定名称 不能为空")
+    @Length(max = 10,message = "绑定名称最长不可超过 10")
     private String bindName;
 
     /**
