453f813d022bd1b0384e05155b26ccfbd739c17e
nacoyer <nacoyer@qq.com>
2024-11-07 16:22:47 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-core/src/main/java/com/starcloud/ops/business/core/config/BusinessAppProperties.java
+++ starcloud-llmops/starcloud-business/starcloud-business-core/src/main/java/com/starcloud/ops/business/core/config/BusinessAppProperties.java
@@ -2,7 +2,6 @@ package com.starcloud.ops.business.core.config;
 
 
 import com.starcloud.ops.business.core.config.translate.TranslatorProperties;
-import com.starcloud.ops.business.core.config.xhs.RedBookProperties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -19,9 +18,4 @@ public class BusinessAppProperties {
      * 翻译服务配置
      */
     private TranslatorProperties translator;
-
-    /**
-     * 小红书配置
-     */
-    private RedBookProperties redBook;
 }
