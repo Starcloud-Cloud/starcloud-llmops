6def0facef17394d5d84bf606a33f2ed4236b4f6
df007df <duanfeisj@gmail.com>
2024-11-12 21:36:09 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-poster/starcloud-business-poster-biz/src/main/java/com/starcloud/ops/business/poster/service/material/MaterialServiceImpl.java
+++ starcloud-llmops/starcloud-business/starcloud-business-poster/starcloud-business-poster-biz/src/main/java/com/starcloud/ops/business/poster/service/material/MaterialServiceImpl.java
@@ -227,7 +227,7 @@ public class MaterialServiceImpl implements MaterialService {
             if (materialGroup == null) {
                 throw exception(MATERIAL_GROUP_NOT_EXISTS);
             }
-            return transform(material, materialGroup.getName(), true);
+            return transform(material, materialGroup.getName(), false);
         } catch (ServiceException exception) {
             throw exception;
         } catch (Exception exception) {
