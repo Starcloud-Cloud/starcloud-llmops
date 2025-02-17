948af2b7c3f7b6e9f76902f2b9492f52f827d5be
nacoyer <nacoyer@qq.com>
2025-02-17 16:32:51 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/api/app/vo/response/config/WorkflowStepWrapperRespVO.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/api/app/vo/response/config/WorkflowStepWrapperRespVO.java
@@ -94,6 +94,7 @@ public class WorkflowStepWrapperRespVO implements Serializable {
         WorkflowStepWrapperRespVO clone = SerializationUtils.clone(handlerStepWrapper);
         if (!"CustomActionHandler".equals(handler)) {
             this.name = clone.getName();
+            this.field = clone.getField();
             this.buttonLabel = clone.getButtonLabel();
             this.description = clone.getDescription();
         }
