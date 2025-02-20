221e6ac4c1802460aa76f7900e41fa8bafe8af66
nacoyer <nacoyer@qq.com>
2025-02-20 18:47:51 +0800

    fix

--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/api/market/vo/response/MarketTemplate.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/api/market/vo/response/MarketTemplate.java
@@ -2,13 +2,17 @@ package com.starcloud.ops.business.app.api.market.vo.response;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author nacoyer
  * @date 2021-06-22
  * @since 1.0.0
  */
 @Data
-public class MarketTemplate {
+public class MarketTemplate implements Serializable {
+
+    private static final long serialVersionUID = -3243386228766373114L;
 
     private String uuid;
 
--- starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/api/market/vo/response/StyleSaleInfo.java
+++ starcloud-llmops/starcloud-business/starcloud-business-app/starcloud-business-app-api/src/main/java/com/starcloud/ops/business/app/api/market/vo/response/StyleSaleInfo.java
@@ -3,13 +3,17 @@ package com.starcloud.ops.business.app.api.market.vo.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author nacoyer
  * @date 2021-06-22
  * @since 1.0.0
  */
 @Data
-public class StyleSaleInfo {
+public class StyleSaleInfo implements Serializable {
+
+    private static final long serialVersionUID = -2634741541905431999L;
 
     @Schema(description = "开启售卖")
     private Boolean openSale;
